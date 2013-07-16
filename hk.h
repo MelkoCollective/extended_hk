d997d55bd09dd15846c31c3ce7f07cdbee715d19
ghwatson <ghwatson@server.fake>
2013-07-16 13:24:23 -0400

    Again, fix tab to whitespace

--- hk.h
+++ hk.h
@@ -2,7 +2,7 @@
 #define HK_H_
 
 int* extended_hoshen_kopelman(const boost::multi_array<int, 2>& nbs,
-															int* occupancy, int N);
+                              int* occupancy, int N);
 int hoshen_kopelman(int **matrix, int m, int n);
 
 #endif /* HK_H_ */
--- test_hk.cpp
+++ test_hk.cpp
@@ -1,3 +1,12 @@
 // TODO: Add some simple testing here.
 
+#include "hk.h"
+#include <boost/multi_array.hpp>
 
+int main() {
+  // Create input data structure.
+
+  // Test.
+  // Verify.
+  return 0;
+}
